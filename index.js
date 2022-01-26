79c2156f07f87bf3af3c0dcab611ae2ec27423a2
LeighMcG83 <leighmcguinness@hotmail.com>
2022-01-26 18:14:14 +0000

    create lab2

--- index.js
+++ index.js
@@ -2,8 +2,47 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('<h1>Hey!! This is my first express app! Whoop Whoop </h1>'))
+app.get('/',  (req, res) => {
+    res.type('text/plain');
+    res.send('Covid Holiday Tours');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!!`))
-c
+// app.get('/about',  (req, res) => {
+//     res.type('text/plain');
+//     res.send('About Our Holidays');
+// });
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+
+app.get('/contact',  (req, res) => {
+    res.type('text/plain');
+    res.send('Don\'t bother we never reply');
+});
+
+app.get('/help', (req, res) => {
+    res.type('text/plain');
+    res.send('Good help is hard to find....Keep looking!!');
+});
+
+// custom 404 page
+app.use( (req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!!`));
