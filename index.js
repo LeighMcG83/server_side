e68d51baa30e28f3cf797fd65a6dcfb88a3cd846
LeighMcG83 <leighmcguinness@hotmail.com>
2022-01-26 19:29:56 +0000

    create task d; complete task a through c

--- index.js
+++ index.js
@@ -4,19 +4,44 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.get('/',  (req, res) => {
-    res.type('text/plain');
-    res.send('Covid Holiday Tours');
+app.get('/', function (req, res) {
+    res.render('home');
 });
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
+
+app.get('/help', function (req, res) {
+    res.render('help');
+});
+
+// // 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+// // 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+// app.get('/',  (req, res) => {
+//     res.type('text/plain');
+//     res.send('Covid Holiday Tours');
+// });
 
 // app.get('/about',  (req, res) => {
 //     res.type('text/plain');
 //     res.send('About Our Holidays');
 // });
-
-app.get('/', function (req, res) {
-    res.render('home');
-});
 
 
 app.get('/contact',  (req, res) => {
@@ -28,21 +53,5 @@ app.get('/help', (req, res) => {
     res.type('text/plain');
     res.send('Good help is hard to find....Keep looking!!');
 });
-
-// custom 404 page
-app.use( (req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-});
-
-// custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
-});
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!!`));
