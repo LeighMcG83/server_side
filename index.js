5480f0e6c408cf719d8988bf9f6621a301629d12
LeighMcG83 <leighmcguinness@hotmail.com>
2022-02-01 06:22:01 +0000

    create lab1; lab2

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+    .create({
+        defaultLayout: 'main'
+    });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.use(express.static('public'));
+
+
+var foil = {
+    "name": "foil",
+    "dob": "01/01/1998",
+    "imageurl": "/images/logo.png"
+};
+
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -20,31 +35,7 @@ app.get('/help', function (req, res) {
     res.render('help');
 });
 
-// // 404 catch-all handler (middleware)
-app.use(function (req, res, next) {
-    res.status(404);
-    res.render('404');
-});
-// // 500 error handler (middleware)
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
-
-
-// app.get('/',  (req, res) => {
-//     res.type('text/plain');
-//     res.send('Covid Holiday Tours');
-// });
-
-// app.get('/about',  (req, res) => {
-//     res.type('text/plain');
-//     res.send('About Our Holidays');
-// });
-
-
-app.get('/contact',  (req, res) => {
+app.get('/contact', (req, res) => {
     res.type('text/plain');
     res.send('Don\'t bother we never reply');
 });
@@ -52,6 +43,23 @@ app.get('/contact',  (req, res) => {
 app.get('/help', (req, res) => {
     res.type('text/plain');
     res.send('Good help is hard to find....Keep looking!!');
+});
+
+app.get('/foil', (req,res) => 
+
+res.render('person', {person: foil} ))
+
+// // 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+// // 500 error handler (middleware)
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!!`));
