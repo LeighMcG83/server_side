800f890fe0448506f82c6058137a50459be27ea0
LeighMcG83 <leighmcguinness@hotmail.com>
2022-02-07 15:58:24 +0000

    wsheet5_task3 complete

--- routes/staff.js
+++ routes/staff.js
@@ -25,20 +25,28 @@ var data = {
 };
 
 
-router.get('/foil', (req, res) =>
-    res.render('person', {
-        person: data.foil
-    }));
+// router.get('/foil', (req, res) =>
+//     res.render('person', {
+//         person: data.foil
+//     }));
 
-router.get('/leigh', (req, res) =>
-    res.render('person', {
-        person: data.leigh
-    }));
+// router.get('/leigh', (req, res) =>
+//     res.render('person', {
+//         person: data.leigh
+//     }));
 
-router.get('/paddy', (req, res) =>
+// router.get('/paddy', (req, res) =>
+//     res.render('person', {
+//         person: data.paddy
+//     }));
+
+router.get('/:name', (req, res) => {
+    var name = req.params.name;
     res.render('person', {
-        person: data.paddy
-    }));
+        person: data[name]
+    })
+})
+
 
 router.get('/', (req, res) =>
     res.render('staff', {
