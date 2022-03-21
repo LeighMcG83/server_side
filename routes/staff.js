08514abb25310f92ce15d4e5de0feab3c61c2243
LeighMcG83 <leighmcguinness@hotmail.com>
2022-03-21 15:26:26 +0000

    create staff edit

--- routes/staff.js
+++ routes/staff.js
@@ -41,10 +41,10 @@ router.get('/:name/edit', async (req, res) => {
     }
 });
 
-router.post('/:name/edit', async (req, res) => {
+router.post('/:name/edit', async (req,res) =>{
 
     await updateStaff(req.body);
-
+    
     res.redirect(303, '/staff');
 
 });
@@ -100,17 +100,14 @@ router.get('/:name/edit', async (req, res) => {
 
     var name = req.params.name;
 
-    const person = await readStaff({
-        'name': name
-    });
+    const person = await readStaff({'name': name});
 
     if (!person) {
         console.log('404 because person doesn\'t exist');
         res.render('404');
-    } else {
-        res.render('personeditform', {
-            person: person
-        });
+    }
+    else {
+        res.render('staffeditform', { person: person });
     }
 });
 
