78fb0f77db0432debf7dc6b7b9e05b4f1e780993
LeighMcG83 <leighmcguinness@hotmail.com>
2022-04-04 14:41:26 +0100

    editstaff flash message

--- routes/staff.js
+++ routes/staff.js
@@ -1,112 +1,122 @@
 // import express js and the router class
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const res = require("express/lib/response");
 const router = express.Router();
 //const mongoose = require('mongoose')
 
-const {
-    readStaff
-} = require('../models/staff');
+const { readStaff } = require("../models/staff");
 
+router.get("/", async (req, res) => {
+  const staff = await readStaff();
 
-router.get('/', async (req, res) => {
-    const staff = await readStaff();
-
-    res.render('listing', {
-        personlist: staff
-    });
-
+  res.render("listing", {
+    personlist: staff,
+  });
 });
-
-
 
 // to edit we first need to fetch the data so we can display in on
 // a form to be edited
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const staff = await readStaff();
+  var newName;
 
-    const staff = await readStaff();
-    var newName;
-
-    if (req.session.staffdata) {
-        newName = req.session.staffdata.name;
-    }
-    else {
-        newName = "";
-    }
-    res.render('listing', { personlist: staff, newName : newName });
+  if (req.session.staffdata) {
+    newName = req.session.staffdata.name;
+  } else {
+    newName = "";
+  }
+  res.render("listing", { personlist: staff, newName: newName });
 });
 
-router.post('/:name/edit', async (req,res) =>{
+router.post("/addnew", async (req, res) => {
+  await createStaff(req.body);
 
-    await updateStaff(req.body);
-    
-    res.redirect(303, '/staff');
-
+  req.session.flash = {
+    type: "success",
+    intro: "Data Saved:",
+    message: "Data for <strong>" + req.body.name + "</strong> has been added",
+  };
+  res.redirect(303, "/staff");
 });
 
+router.get("/:name/edit", async (req, res) => {
+  var name = req.params.name;
 
-router.post('/addnew', async (req, res) => {
+  const person = await readStaff({ name: name });
 
-    // note we leave error handling for now and assume our data is created.
-    // note: this is not safe code. Any inputs from a user should be validated and sanitised before
-    // being saved to the database.
-
-    await createStaff(req.body);
-    req.session.staffdata = { name: req.body.name};
-    res.redirect(303, '/staff');
-
+  if (!person) {
+    console.log("404 because person doesn't exist");
+    res.render("404");
+  } else {
+    res.render("personeditform", { person: person });
+  }
 });
 
+router.post("/:name/edit", async (req, res) => {
+  await updateStaff(req.body);
 
+  req.session.flash = {
+    type: "success",
+    intro: "Data Updated:",
+    message: "Data for <strong>" + req.body.name + "</strong> has been updated",
+  };
 
-router.get('/addnew', async (req, res) => {
-    res.render('personform');
+  res.redirect(303, "/staff");
 });
 
+router.get("/addnew", async (req, res) => {
+  res.render("personform");
+});
 
-router.get('/:name', async (req, res) => {
-    var name = req.params.name;
+router.post("/addnew", async (req, res) => {
+  await createStaff(req.body);
 
-    const person = await readStaff({
-        'name': name
+  req.session.flash = {
+    type: "success",
+    intro: "Data Saved:",
+    message: "Data for <strong>" + req.body.name + "</strong> has been added",
+  };
+  res.redirect(303, "/staff");
+});
+
+router.get("/:name", async (req, res) => {
+  var name = req.params.name;
+
+  const person = await readStaff({
+    name: name,
+  });
+
+  if (!person) {
+    console.log("404 because person doesn't exist");
+    res.render("404");
+  } else {
+    res.render("person", {
+      person: person,
     });
-
-    if (!person) {
-        console.log('404 because person doesn\'t exist');
-        res.render('404');
-    } else {
-        res.render('person', {
-            person: person
-        });
-    }
+  }
 });
 
 // no error checking for now.
 //
-router.get('/:name/delete', async (req, res) => {
-    var name = req.params.name;
+router.get("/:name/delete", async (req, res) => {
+  var name = req.params.name;
 
-    await deleteStaff(name);
+  await deleteStaff(name);
 
-    res.redirect(303, '/staff');
-
+  res.redirect(303, "/staff");
 });
 
-router.get('/:name/edit', async (req, res) => {
+router.post("/:name/delete", async (req, res) => {
+  await deleteStaff(req.body.name);
 
-    var name = req.params.name;
+  req.session.flash = {
+    type: "success",
+    intro: "Data Deleted:",
+    message: "Data for <strong>" + req.body.name + "</strong> has been deleted",
+  };
 
-    const person = await readStaff({'name': name});
-
-    if (!person) {
-        console.log('404 because person doesn\'t exist');
-        res.render('404');
-    }
-    else {
-        res.render('staffeditform', { person: person });
-    }
+  res.redirect(303, "/staff");
 });
-
 
 module.exports = router;
